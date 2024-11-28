e46338baf7d8a7a3fa8d9d9b0f3ec01d72641af2
sin young261 <waterfired@naver.com>
2024-11-28 10:14:11 +0900

    init

---
+++ HellowWorld/src/com/yedam/Hello.java
@@ -0,0 +1,27 @@
+package com.yedam;
+
+//대소문자 구문(A-a 서로 다름)
+//객체지향언이(Object Oriented Language)
+public class Hello {
+
+	// 메소드 여러개를 선언. start(), main();
+
+
+public static void start(){
+	System.out.println("시작합니다");
+}
+
+//메인 메소드, 함수(function)
+public static void main(String[] args){
+//콘솔에 Hello, World 출력
+System.out.println("Hello, world");
+start();
+
+//let result = "Hello";
+OperatopmExe exe = new OperatopmExe();
+exe.sum(10, 20);
+//Hello.java -> Hello.class(실행파일)
+//컴파일(저장하면 eclipse 컴파일)
+//HelloWorld프로젝-HelloWorld repository
+}
+}//end of class.
